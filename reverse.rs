76dcfa1b211e78dee5c47439a38360de4ace3acf
Marisa Goode <78165523+dev-marisa@users.noreply.github.com>
2022-02-22 18:55:03 -0500

    initial

---
+++ reverse.rs
@@ -0,0 +1,21 @@
+fn reverse_string(word : &str) -> String {
+    let mut reversed = String::new();
+    for c in word.chars().rev() {
+        reversed.push(c);
+    }
+    return reversed;
+}
+
+fn reverse_array(arr: &mut [i32]) -> &mut [i32] {
+    for i in 0..arr.len()/2 {
+        let temp = arr[i];
+        arr[i] = arr[arr.len()-1-i];
+        arr[arr.len()-1-i] = temp;        
+    }
+    return arr;
+}
+
+fn main() {
+    println!("{}", reverse_string("hello"));
+    println!("{:?}", reverse_array(&mut [1, 2, 3, 4, 5]));
+}
